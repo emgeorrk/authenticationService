69886b18d4f65e029a2de288d746d5f9ef40c290
Egor <nounstep@gmail.com>
2024-08-10 17:38:15 +0300

    Feature: new fields in db

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -27,7 +27,13 @@ func NewStorage(storage config.Storage) (*Storage, error) {
 	CREATE TABLE IF NOT EXISTS users (
     	id UUID PRIMARY KEY,
     	name VARCHAR(255) NOT NULL,
-    	email VARCHAR(255) UNIQUE NOT NULL);
+    	email VARCHAR(255) UNIQUE NOT NULL,
+    	max_active_token_pairs INT NOT NULL DEFAULT 5,
+    	access_token_lifetime_minutes INT NOT NULL DEFAULT 60,
+    	refresh_token_lifetime_minutes INT NOT NULL DEFAULT 129600,
+	    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		CONSTRAINT check_token_lifetime CHECK (access_token_lifetime_minutes <= users.refresh_token_lifetime_minutes));
 	`)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
@@ -38,16 +44,17 @@ func NewStorage(storage config.Storage) (*Storage, error) {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	// TODO: создать индекс на token_hash
 	stmt, err = db.Prepare(`
-	CREATE TABLE IF NOT EXISTS refresh_tokens (
-    	id UUID PRIMARY KEY,
+	CREATE TABLE IF NOT EXISTS tokens (
+    	jti UUID PRIMARY KEY,
     	user_id UUID NOT NULL REFERENCES users(id),
     	refresh_token_hash VARCHAR(255) NOT NULL,
     	ip_address VARCHAR(39) NOT NULL,
-	    status TEXT NOT NULL CHECK (status IN ('used', 'unused')),
+	    refresh_token_status TEXT NOT NULL CHECK (refresh_token_status IN ('used', 'unused')),
     	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    	expires_at TIMESTAMP WITH TIME ZONE NOT NULL);
+    	access_token_expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    	refresh_token_expires_at TIMESTAMP WITH TIME ZONE NOT NULL);
+		CREATE INDEX idx_refresh_token_hash ON tokens(refresh_token_hash);
 	`)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
@@ -98,7 +105,7 @@ func (s *Storage) GetUserByID(id string) (*models.User, error) {
 func (s *Storage) CreateRefreshToken(refreshToken *models.RefreshToken) (string, error) {
 	const op = "storage.postgres.CreateRefreshToken"
 
-	stmt, err := s.db.Prepare("INSERT INTO refresh_tokens (id, user_id, refresh_token_hash, ip_address, status, created_at, expires_at) VALUES ($1, $2, $3, $4, $5, $6, &7)")
+	stmt, err := s.db.Prepare("INSERT INTO tokens (id, user_id, refresh_token_hash, ip_address, status, created_at, expires_at) VALUES ($1, $2, $3, $4, $5, $6, &7)")
 	if err != nil {
 		return "", fmt.Errorf("%s: %w", op, err)
 	}
@@ -115,7 +122,7 @@ func (s *Storage) CreateRefreshToken(refreshToken *models.RefreshToken) (string,
 func (s *Storage) GetRefreshTokenByID(id string) (*models.RefreshToken, error) {
 	const op = "storage.postgres.GetRefreshTokenByID"
 
-	stmt, err := s.db.Prepare("SELECT id, user_id, refresh_token_hash, ip_address, status, created_at, expires_at FROM refresh_tokens WHERE id = $1")
+	stmt, err := s.db.Prepare("SELECT id, user_id, refresh_token_hash, ip_address, status, created_at, expires_at FROM tokens WHERE id = $1")
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
@@ -131,7 +138,7 @@ func (s *Storage) GetRefreshTokenByID(id string) (*models.RefreshToken, error) {
 func (s *Storage) UpdateRefreshTokenStatus(id, newStatus string) error {
 	const op = "storage.postgres.UpdateRefreshTokenStatus"
 
-	stmt, err := s.db.Prepare("UPDATE refresh_tokens SET status = $1 WHERE id = $2")
+	stmt, err := s.db.Prepare("UPDATE tokens SET status = $1 WHERE id = $2")
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
