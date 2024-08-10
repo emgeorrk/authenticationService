07353e9042838083e0f0e5cbdd10a1fcc12c10ec
Egor <nounstep@gmail.com>
2024-08-10 15:47:35 +0300

    Feature: storage: interface, const errors

--- storage/storage.go
+++ storage/storage.go
@@ -1,5 +1,18 @@
 package storage
 
-// TODO: описать ошибки
+import (
+	"authenticationService/models"
+)
 
-// TODO: описать интерфейс
+const (
+	UserNotFound         = "user not found"
+	RefreshTokenNotFound = "refresh token not found"
+)
+
+type TokenKeeper interface {
+	CreateUser(user *models.User) (string, error)
+	CreateRefreshToken(refreshToken *models.RefreshToken) (string, error)
+	GetUserByID(id string) (*models.User, error)
+	GetRefreshTokenByID(id string) (*models.RefreshToken, error)
+	UpdateRefreshTokenStatus(id, newStatus string) error
+}
