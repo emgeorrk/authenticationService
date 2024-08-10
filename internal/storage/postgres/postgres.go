47d1a91921708f60c2087a2e378b07b6c1cc4e69
Egor <nounstep@gmail.com>
2024-08-10 15:59:30 +0300

    Fix: missing field "users.name" in database initialization

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -26,6 +26,7 @@ func NewStorage(storage config.Storage) (*Storage, error) {
 	stmt, err := db.Prepare(`
 	CREATE TABLE IF NOT EXISTS users (
     	id UUID PRIMARY KEY,
+    	name VARCHAR(255) NOT NULL,
     	email VARCHAR(255) UNIQUE NOT NULL);
 	`)
 	if err != nil {
