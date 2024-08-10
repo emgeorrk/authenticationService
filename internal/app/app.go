05a6f13d4f4bac7694ad299c0f1ec1a452b48598
Egor <nounstep@gmail.com>
2024-08-10 15:50:04 +0300

    Feature: struct for application

---
+++ internal/app/app.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"authenticationService/internal/config"
+	"authenticationService/storage"
+	"log/slog"
+)
+
+type App struct {
+	Config  *config.Config
+	Logger  *slog.Logger
+	Storage storage.TokenKeeper
+}
+
+func New(config *config.Config, storage storage.TokenKeeper, logger *slog.Logger) *App {
+	return &App{
+		Config:  config,
+		Logger:  logger,
+		Storage: storage,
+	}
+}
